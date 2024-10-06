ce3a1ff356500cef910e1fd6d8774816ba9f6348
sifat1234 <ahahmedsifat111@gmail.com>
2024-10-07 02:36:10 +0600

    4rth time

--- src/MidContent.jsx
+++ src/MidContent.jsx
@@ -123,9 +123,8 @@ function AboutSection() {
                   <div className="w-full px-2 sm:w-1/2 sm:px-4 lg:px-2 xl:px-4">
                     <div className="mb-4 sm:mb-8 sm:h-[400px] md:h-[540px] lg:h-[400px] xl:h-[500px]">
                       <LogoPic
-                        // src="/src/assets/images/about/about-image-01.jpg"
-                        src="/src/assets/images/about/about-image-01.jpg"
-
+                       
+                        src="/assets/images/about/about-image-01.jpg"
                         alt="about image"
                         className="h-full w-full object-cover object-center"
                       />
