8d5b9358e363d23fc3f7a7ba6b8f17390ef5e45f
sifat1234 <ahahmedsifat111@gmail.com>
2024-10-07 02:31:20 +0600

    commited

--- src/LogoPic.jsx
+++ src/LogoPic.jsx
@@ -1,9 +1,7 @@
 // eslint-disable-next-line react/prop-types
 export default function LogoPic({src, alt, className}) {
-    return(
-        <>
-             <img src={src} alt={alt} className={className} />
-            
-        </>
+    return(    <img src={src} alt={alt} className={className} />
+   
+       
     )
 }
